41ef2ca61ba2806c1d383110a4f8378c97d108da
Brodey Newman <brodeynewman@gmail.com>
2018-10-12 20:43:45 -0400

    modifying shuffle

--- packages/shuffle/src/index.js
+++ packages/shuffle/src/index.js
@@ -3,30 +3,14 @@
  * @param {array} arr - the array to be shuffled
  * @returns {array} - shuffled array
  */
-function shuffle(arr) {
-  if (arr === null || !arr.length) return [];
-
-  let top = arr.length;
-    rand = top,
-    curr = top;
-
+module.exports = function shuffle(arr) {
+  if (!Array.isArray(arr) || !arr.length) return [];
+  let dupe = arr, top = arr.length, rand = top, curr = top;
   while (--top) {
-    rand = Math.floor(Math.random() * (top + 1));
-    curr = arr[rand];
-    arr[rand] = arr[top];
-    arr[top] = curr;
+    rand = (Math.random() * (top + 1)) | 0;
+    curr = dupe[rand];
+    dupe[rand] = dupe[top];
+    dupe[top] = curr;
   }
-
-  return arr;
+  return dupe;
 }
-
-module.exports = shuffle;
-
-// feel free to run this for speed test
-const now = Date.now();
-const amt = 10000;
-for (let i = 0; i < amt; i++) {
-  shuffle(Array(i));
-}
-
-console.log(`Finished shuffling ${amt} times. Process took: ${Date.now() - now}ms`);
