beafbd0295ac37ec439569d92766310c393f550b
Brodey Newman <brodeynewman@gmail.com>
2018-10-13 19:55:29 -0400

    making shuffle faster

--- packages/shuffle/src/index.js
+++ packages/shuffle/src/index.js
@@ -1,19 +1,40 @@
 /**
- * Shuffle function which implements the "Fisher-Yates" shuffle algorithm
- * @param {array} arr - the array to be shuffled
- * @returns {array} - shuffled array
+ * Shallow clones an array
+ * @param {Array} arr - the array to clone
  */
-function shuffle(arr) {
-  if (!Array.isArray(arr) || !arr.length) return [];
-  let dupe = arr.slice(), top = arr.length, rand = top, curr = top;
+function cloneArray(arr) {
+  let clone = new Array(arr.length), ind = arr.length;
+  while (ind--) {
+    clone[ind] = arr[ind];
+  }
+  return clone;
+}
+
+/**
+ * Implements the Fisher-Yates" shuffle algorithm
+ * @param {Array} arr - the array to be shifted
+ * @returns {Array} - the newly shifted array
+ */
+function implementShuffle(arr) {
+  let dupe = cloneArray(arr), top = arr.length, rand = top, curr = top;
   while (--top) {
     rand = (Math.random() * (top + 1)) | 0;
     curr = dupe[rand];
     dupe[rand] = dupe[top];
     dupe[top] = curr;
   }
-
   return dupe;
+}
+
+/**
+ * Shuffle function which implements the "Fisher-Yates" shuffle algorithm
+ * @param {array} arr - the array to be shuffled
+ * @returns {array} - shuffled array
+ */
+function shuffle(arr) {
+  const size = arr && arr.length;
+  if (size > 1) return implementShuffle(arr);
+  return (size && size > 1) ? new Array(1).fill(arr[0]) : new Array();
 }
 
 module.exports = shuffle;
