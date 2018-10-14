70e85bc6b509f7bbfc3dfc4ea483e6b1f329b479
Bobby Smedley <rsmedley17@live.com>
2018-10-14 12:17:28 -0400

    Finished head function

--- packages/head/src/index.js
+++ packages/head/src/index.js
@@ -1,14 +1,12 @@
 /**
  * Head function, which returns the first index of a proven array
- * @param {array} arr - the array that will provide the first index
- * @returns array[0] - the value at the first index of an array
+ * @param {Array} arr - the array that will provide the first index
+ * @returns {Array} - the value at the first index of an array
  */
 
 
 function head(array) {
-  if (array && array.length) {
-    return array[0];
-  } return undefined;
+  if (array && array.length) return array[0];
+  return undefined;
 }
-
 module.exports = head;
