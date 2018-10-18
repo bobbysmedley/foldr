71d7df4eea6a2001137a7d164ddabc88ede96d1f
Bobby Smedley <rsmedley17@live.com>
2018-10-17 22:14:44 -0400

    corrected if nothing was passed to dropNum parameter, should return original array

--- packages/drop/src/index.js
+++ packages/drop/src/index.js
@@ -7,12 +7,19 @@
 
 function drop(array, dropNum) {
   if (!array || !array.length) return new Array();
-  let newArr = new Array(), i = 0;
-  while ((i + dropNum) < array.length){
-    newArr[i] = array[dropNum + i];
-    i++
+
+  function isInteger(int) {
+    return (typeof int === 'number') && (int % 1 === 0);
   }
+  if (isInteger(dropNum)){
+    let newArr = new Array(), i = 0;
+    while ((i + dropNum) < array.length){
+      newArr[i] = array[dropNum + i];
+      i++
+    }
     return newArr;
+  }
+  else return array;
 }
 
 module.exports = drop;
